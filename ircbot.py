0c82105038271a14734809c2da6e882b227559f5
nathanvy <nathan@nathan.pro>
2021-03-25 00:39:15 -0700

    first prototype

--- ircbot.py
+++ ircbot.py
@@ -3,13 +3,46 @@
 import sys
 import irc.bot
 import irc.strings
+import tempora
+import numpy
+import time
+import logging
+import RPi.GPIO as GPIO
 from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 
+#these are in seconds as a proxy for the time constant tau
+sufficientlywet = 0.005
+sufficientlydry = 0.001
+
+measurementPin = 24
+chargePin = 23
+
+begintime = 0
+    
 class LeetBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port, "hippo")], nickname, nickname)
         self.channel = channel
 
+        self.scan_interval = 60 #seconds
+        s = self.reactor.scheduler
+        s.execute_every( self.scan_interval, self.startscan )
+
+        GPIO.add_event_callback( measurementPin, callback=lambda x: self.edge_callback() )
+
+    #where the sausage gets made
+    def startscan(self):
+        global begintime
+        begintime = time.clock_gettime(time.CLOCK_MONOTONIC)
+        GPIO.output( chargePin, GPIO.HIGH ) #commence charge, callback will fire when it hits 2 volts
+
+    def edge_callback(self):
+        global begintime
+        t = time.clock_gettime(time.CLOCK_MONOTONIC)
+        GPIO.output( chargePin, GPIO.LOW ) #reset it for the next run
+        self.connection.privmsg( self.channel, "I measured {first} and {second} for t2 - t1 = {ans}".format(first=t, second=begintime, ans=t-begintime ))
+        begintime = 0        
+        
     def on_nicknameinuse( self, c, e ):
         c.nick(c.get_nickname() + "_")
 
@@ -30,7 +63,7 @@ class LeetBot(irc.bot.SingleServerIRCBot):
         nick = e.source.nick
         c = self.connection
 
-        c.privmsg("Commands are disabled")
+        c.privmsg( self.channel, "Commands are disabled")
         
         # if cmd == "disconnect":
         #     self.disconnect()
@@ -57,8 +90,31 @@ class LeetBot(irc.bot.SingleServerIRCBot):
         # else:
         #     c.notice(nick, "Not understood: " + cmd)
 
+
+
+# so you have something like
+#   GPIO23                      GPIO24                                        GND
+#     |                                        |                                                   |
+#     +-----[47kOhm]------+---------| capacitor  |------+
+#
+# and you want to measure when the voltage drop across the resistor is 0 because that means
+# that your capacitor is charged.  Capacitors in this configuration obey the equation V(t) = V0 * (1 - e^[-t/tau]) where tau is the "RC constant"
+#
+# Conveniently, for a charging capacitor, 1 - e^-1 = 0.632 or 63.2% and it just so happens that 63.2% of 3V3 CMOS logic is 2.08V which loosely corresponds
+# to the minimum input voltage to be considered a "high" which is 2 volts.
+
 def main():
+    print("Raspi Gardener version 1.3.3.7 initializing...")
+    #GPIO.setwarnings( False )
+    GPIO.setmode( GPIO.BCM ) #so-called "broadcom numbering" which is logical numbering not physical pins
+
+    GPIO.setup( chargePin, GPIO.OUT)
+    GPIO.setup( measurementPin, GPIO.IN )
+    GPIO.output( chargePin, GPIO.LOW )
+    GPIO.add_event_detect( measurementPin, GPIO.RISING )
+    
     bot = LeetBot("#bots", "raspi3bplus", "irc.squishynet.net", 6667)
+    print("Initialization complete.  Connecting to IRC.")
     bot.start()
 
 
